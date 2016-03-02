016d42b2c926edf35daaaab29129cd2c797455d6
nlaz <nikolazaris@utexas.edu>
2016-03-02 00:49:58 -0600

    initial commit

---
+++ app.rb
@@ -0,0 +1,57 @@
+require 'sinatra'
+require "sinatra/multi_route"
+require 'httparty'
+
+require 'dotenv'
+Dotenv.load
+
+class ExampleApp < Sinatra::Base
+  register Sinatra::MultiRoute
+
+  # Config
+  callback = 'http://localhost:9292/auth/mlh/callback'
+  client_id = ENV['MYMLH_APP_ID']
+  client_secret = ENV['MYMLH_SECRET']
+
+  # Helpers
+
+  def fetch_user(access_token)
+    base_url = "https://my.mlh.io/api/v1/user"
+    qs = URI.encode_www_form({'access_token': access_token}
+
+    JSON.parse(HTTParty.get("#{base_url}?#{qs}"))
+  end
+
+  get '/' do
+    "<a href='/auth/mlh'>Login with MLH</a>"
+  end
+
+  get '/auth/mlh' do  
+    base_url = "https://my.mlh.io/oauth/authorize"
+
+    qs = URI.encode_www_form(
+      'client_id': client_id,
+      'redirect_uri': callback,
+      'response_type': 'code'
+    )
+
+    redirect "#{base_url}?#{qs}"
+  end
+
+  route :get, :post, '/auth/mlh/callback' do
+    base_url = "https://my.mlh.io/oauth/token"
+    code = params[:code]
+
+    headers = { 'Content-Type' => 'application/json' }
+    body = {
+      'client_id': client_id,
+      'client_secret': client_secret,
+      'code': code,
+      'grant_type': 'authorization_code',
+      'redirect_uri': callback
+    }.to_json
+
+    resp = HTTParty.post( base_url, body: body, headers: headers )
+    puts resp['access_token']
+  end
+end
