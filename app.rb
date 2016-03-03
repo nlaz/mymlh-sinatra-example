1d81966b348568143c53affa066ed68ceecdec14
nlaz <nikolazaris@utexas.edu>
2016-03-03 16:00:49 -0600

    add comments

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class ExampleApp < Sinatra::Base
   register Sinatra::MultiRoute
 
   # Config
-  callback = 'http://localhost:9292/auth/mlh/callback'
+  callback = ENV['MYMLH_CALLBACK']
   client_id = ENV['MYMLH_APP_ID']
   client_secret = ENV['MYMLH_SECRET']
 
@@ -20,15 +20,20 @@ class ExampleApp < Sinatra::Base
     base_url = "https://my.mlh.io/api/v1/user"
     qs = URI.encode_www_form({'access_token': access_token})
 
-
+    # Tokens don't expire, but users can reset them, so best to do some error
+    # handling here too.
     HTTParty.get("#{base_url}?#{qs}").to_json
   end
+
+  # Routes
 
   get '/' do
     "<a href='/auth/mlh'>Login with MLH</a>"
   end
 
   get '/auth/mlh' do  
+    # Step 1: Request an Authorization Code from My MLH by directing a user to
+    # your app's authorize page.
     base_url = "https://my.mlh.io/oauth/authorize"
 
     qs = URI.encode_www_form(
@@ -41,6 +46,8 @@ class ExampleApp < Sinatra::Base
   end
 
   route :get, :post, '/auth/mlh/callback' do
+    # Step 2: Assuming the user clicked authorize, we should get an Authorization
+    # Code which we can now exchange for an access token.
     base_url = "https://my.mlh.io/oauth/token"
     code = params[:code]
 
@@ -52,6 +59,10 @@ class ExampleApp < Sinatra::Base
       'grant_type': 'authorization_code',
       'redirect_uri': callback
     }.to_json
+
+    # Step 3: Now we should have an access token which we can use to get the
+    # current user's profile information.  In a production app you would
+    # create a user and save it to your database at this point.
 
     resp = HTTParty.post( base_url, body: body, headers: headers )
     user = fetch_user(resp['access_token'])
